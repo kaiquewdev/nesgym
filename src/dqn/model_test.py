da707b13e243dc9b16ee7d5184eaf1a1bc4d4a58
Kaique da Silva <kaique.silva.dev@gmail.com>
2018-04-09 06:18:53 -0300

    Changed some inputs of the previously test and testing a model instance existence

--- src/dqn/model_test.py
+++ src/dqn/model_test.py
@@ -5,12 +5,17 @@ from model import q_function
 class QFunctionImageInputTest(tf.test.TestCase):
 	def instanceComparison(self):
 		with self.test_session():
-			self.assertEqual(q_function((84,84,1),10).__class__,tf.keras.layers.Input)
+			self.assertEqual(q_function((84,84,1),16).__class__,tf.keras.layers.Input)
 
 class QFunctionActionsTest(tf.test.TestCase):
 	def instanceComparison(self):
 		with self.test_session():
-			self.assertEqual(q_function((84,84,1),10).__class__,tf.keras.layers.Dense)
+			self.assertEqual(q_function((84,84,1),16).__class__,tf.keras.layers.Dense)
+
+class QModelTest(tf.test.TestCase):
+	def instanceComparison(self):
+		with self.test_session():
+			self.assertEqual(q_model((84,84,1),16).__class__,tf.keras.models.Model)
 
 if __name__ == '__main__':
 	tf.test.main()
