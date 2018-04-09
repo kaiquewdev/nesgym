ca74ebb7b93fa14681732726503c529cb5d45a03
Kaique da Silva <kaique.silva.dev@gmail.com>
2018-04-09 07:17:40 -0300

    Fixed the test with a convenient pattern

--- src/dqn/model_test.py
+++ src/dqn/model_test.py
@@ -1,21 +1,36 @@
 import tensorflow as tf
 
 from model import q_function
+# from model import q_model
+# from model import DoubleDQN
+
+import numpy as np
 
 class QFunctionImageInputTest(tf.test.TestCase):
-	def instanceComparison(self):
+	def testInstanceComparison(self):
 		with self.test_session():
-			self.assertEqual(q_function((84,84,1),16).__class__,tf.keras.layers.Input)
+			q = q_function((84,84,1),16)
+			self.assertEqual(type(q[0]),tf.Tensor)
 
 class QFunctionActionsTest(tf.test.TestCase):
 	def instanceComparison(self):
 		with self.test_session():
-			self.assertEqual(q_function((84,84,1),16).__class__,tf.keras.layers.Dense)
+			q = q_function((84,84,1),16)
+			self.assertEqual(q[1],tf.Tensor)
 
-class QModelTest(tf.test.TestCase):
-	def instanceComparison(self):
-		with self.test_session():
-			self.assertEqual(q_model((84,84,1),16).__class__,tf.keras.models.Model)
+# class QModelTest(tf.test.TestCase):
+# 	def instanceComparison(self):
+# 		with self.test_session():
+# 			self.assertEqual(q_model((84,84,1),16).__class__,tf.keras.models.Model)
+
+# class DoubleDQNClassTest(tf.test.TestCase):
+# 	def setUp(self):
+# 		self.dqn = DQN(image_shape=(84, 84, 1),
+#                        num_actions=16,
+#                        training_starts=10000,
+#                        target_update_freq=4000,
+#                        training_batch_size=64,
+#                        exploration=exploration_schedule)
 
 if __name__ == '__main__':
 	tf.test.main()
