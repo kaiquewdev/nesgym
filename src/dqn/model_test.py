b303b434ae3411f706da9ee1500032d8403575d9
Kaique da Silva <kaique.silva.dev@gmail.com>
2018-04-09 09:24:14 -0300

    Including the coverage of method tests for DoubleDQN

--- src/dqn/model_test.py
+++ src/dqn/model_test.py
@@ -4,9 +4,7 @@ from keras.models import Model
 
 from model import q_function
 from model import q_model
-# from model import DoubleDQN
-
-import numpy as np
+from model import DoubleDQN
 
 class QFunctionImageInputTest(tf.test.TestCase):
 	def testInstanceComparison(self):
@@ -26,14 +24,17 @@ class QModelTest(tf.test.TestCase):
 			q = q_model((84,84,1),16)
 			self.assertEqual(type(q),Model)
 
-# class DoubleDQNClassTest(tf.test.TestCase):
-# 	def setUp(self):
-# 		self.dqn = DQN(image_shape=(84, 84, 1),
-#                        num_actions=16,
-#                        training_starts=10000,
-#                        target_update_freq=4000,
-#                        training_batch_size=64,
-#                        exploration=exploration_schedule)
+class DoubleDQNClassTest(tf.test.TestCase):
+	def setUp(self):
+		self.dqn = DoubleDQN(image_shape=(84, 84, 1),
+                       num_actions=16,
+                       training_starts=10000,
+                       target_update_freq=4000,
+                       training_batch_size=64,
+                       exploration=100)
+
+	def testInstanceComparison(self):
+		self.assertEqual(type(self.dqn),DoubleDQN)
 
 if __name__ == '__main__':
 	tf.test.main()
