0cd2891dcdfebb90525e3d35c8f19d4e5e5fae48
Kaique da Silva <kaique.silva.dev@gmail.com>
2018-04-08 22:57:24 -0300

    Covered another small case for a convenience between classes

--- src/dqn/model_test.py
+++ src/dqn/model_test.py
@@ -7,5 +7,10 @@ class QFunctionImageInputTest(tf.test.TestCase):
 		with self.test_session():
 			self.assertEqual(q_function((84,84,1),10).__class__,tf.keras.layers.Input)
 
+class QFunctionActionsTest(tf.test.TestCase):
+	def instanceComparison(self):
+		with self.test_session():
+			self.assertEqual(q_function((84,84,1),10).__class__,tf.keras.layers.Dense)
+
 if __name__ == '__main__':
 	tf.test.main()
